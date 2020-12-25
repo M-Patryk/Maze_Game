0dccf5e57ee3a8c44ffb5dbf1cefa531780df3a0
Patryk <47945758+M-Patryk@users.noreply.github.com>
2020-12-25 21:34:31 +0100

    Added arrays as a foundation of the maze generation

--- index.js
+++ index.js
@@ -7,51 +7,36 @@ const { Engine, Render, World, Bodies, Runner, MouseConstraint, Mouse } = Matter
 // MouseConstraint - Mouse input
 const engine = Engine.create();
 const { world } = engine;
+
+const cells = 3
+const height = 600;
+const width = 600;
 const render = Render.create({
 	// Inside of here I want to tell the render where I want to show my representation of everything inside of my HTML doc
 	// Element is doc.body || So "go to render, the representation of world iside of doc.body"
 	element : document.body,
 	engine  : engine,
 	options : {
-		wireframes: false,
-		width  : document.body.clientWidth,
-		height : 850
+		wireframes : false,
+		width,
+		height
 	}
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
-World.add(
-	world,
-	MouseConstraint.create(engine, {
-		mouse : Mouse.create(render.canvas)
-	})
-);
 
 //Walls
 const walls = [
-	Bodies.rectangle(document.body.clientWidth / 2, 0, document.body.clientWidth, 40, { isStatic: true }),
-	Bodies.rectangle(document.body.clientWidth / 2, render.options.height, document.body.clientWidth, 40, {
-		isStatic : true
-	}),
-	Bodies.rectangle(0, render.options.height / 2, render.options.height, 40, { isStatic: true, angle: 1.5707963268 }),
-	Bodies.rectangle(document.body.clientWidth, render.options.height / 2, render.options.height, 40, {
-		isStatic : true,
-		angle    : 1.5707963268
-	})
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }), //Top wall
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), //Bottom wall
+	Bodies.rectangle(0, height / 2, height, 40, { isStatic: true, angle: 1.5707963268 }),
+	Bodies.rectangle(width, height / 2, height, 40, { isStatic: true, angle: 1.5707963268 })
 ];
 World.add(world, walls);
 
-//Random shapes
-for (let i = 0; i < 50; i++) {
-	if(Math.random() > 0.5){
-		World.add(
-			world,
-			Bodies.rectangle(Math.random() * render.options.width, Math.random() * render.options.height, 50, 50)
-		);
-	} else {
-		World.add(world, Bodies.circle(Math.random() * render.options.width, Math.random() * render.options.height, 35, {render: {
-			fillStyle: 'grey'
-		}}))
-	}
-}
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false)) //.fill uzupelnia wszystkie elementy w tablicy zaczynajac od indexu 0
+//Tworzy tablice od dlugosci cells i kazdy element po kolei mapuje jako nowa tablica o dlugosci cells i jej kazdy element ma wartosc false
+
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
+const horizontals = Array(cells - 1).fill(null).map(()=> Array(cells).fill(false))
